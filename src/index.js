9ffd0eef84b30691ddc7ddbda4c8053ea8e873cf
snehapatra454 <snehapatra358@gmail.com>
2024-08-06 12:47:58 +0530

    commits

--- src/index.js
+++ src/index.js
@@ -9,22 +9,22 @@ import App from "./components/description";
 //import App from "./components/Loginpage";
 //import App from "./components/ProfilePage";
 //import App from "./components/ContactPage";
-/*import App from "./components/admin_description";
-import { BrowserRouter } from "react-router-dom";
-import ContextComponent from "./components/RouteComponent";
-import reportWebVitals from "./reportWebVitals";*/
+//import App from "./components/admin_description";
+//import { BrowserRouter } from "react-router-dom";
+//import ContextComponent from "./components/RouteComponent";
+//import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+/*root.render(
   <React.StrictMode>
     <BrowserRouter basename={window.location.pathname || ''}>
-      <App />
+      <ContextComponent />
     </BrowserRouter>
   </React.StrictMode>
-);
-/*ReactDOM.render(
+);*/
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")*/
-//);
+  document.getElementById("root")
+);
